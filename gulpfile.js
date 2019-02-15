67eae38939145201b05f00cd746f1bab91e39752
Papagoat <giantnookie@hotmail.com>
2019-02-16 03:27:25 +0800

    netlify deploy

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 const { watch, series, parallel, src, dest, gulp } = require('gulp');
 const connect = require('gulp-connect'); // Runs a local webserver
 const open = require('gulp-open'); // Opens a URL in a web browser
+const exec = require('child_process').exec; // run command-line programs from gulp
+const execSync = require('child_process').execSync; // command-line reports
 
 // Launch Chrome web browser
 // https://www.npmjs.com/package/gulp-open
@@ -23,6 +25,29 @@ function server(done) {
   });
   done();
 }
+
+// Commit and push files to Git
+function git(done) {
+  return exec('git add . && git commit -m "netlify deploy" && git push');
+  done();
+}
+
+// Watch for netlify deployment
+function netlify(done) {
+  return new Promise(function(resolve, reject) {
+      console.log(execSync('netlify watch').toString());
+      resolve();
+    });
+}
+
+// Preview Deployment
+function netlifyOpen(done) {
+  return exec('netlify open:site');
+  done();
+}
+
+// Deploy command
+exports.deploy = series(git, netlify, netlifyOpen);
 
 // Default Gulp command
 exports.default = series(openBrowser, server);
